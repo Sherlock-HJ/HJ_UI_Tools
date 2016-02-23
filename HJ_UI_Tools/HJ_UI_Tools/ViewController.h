18aa767a82351b68f7e03d199d615ce38eecfe07
吴宏佳 <wuhongjia@wuhongjias-mini.lan>
2016-02-23 16:08:14 +0800

    初始化

---
+++ HJ_UI_Tools/HJ_UI_Tools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJ_UI_Tools
+//
+//  Created by 吴宏佳 on 16/2/23.
+//  Copyright © 2016年 吴宏佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HJ_UI_Tools/HJ_UI_ToolsTests/HJ_UI_ToolsTests.m
@@ -0,0 +1,39 @@
+//
+//  HJ_UI_ToolsTests.m
+//  HJ_UI_ToolsTests
+//
+//  Created by 吴宏佳 on 16/2/23.
+//  Copyright © 2016年 吴宏佳. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HJ_UI_ToolsTests : XCTestCase
+
+@end
+
+@implementation HJ_UI_ToolsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
